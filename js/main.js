80979c8c3b08c42d21f709ac756d0d5496e9f26d
ArtIce15 <artice435@gmail.com>
2025-02-06 10:28:28 +0700

    123

--- js/main.js
+++ js/main.js
@@ -2,10 +2,38 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         image: "./assets/vmSocks-green-onWhite.jpg",
         description: "A pair of warm, fuzzy socks.",
         altText: "A pair of socks",
-        inStock: true,
+        inStock: false,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-    }
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+
+    },
+
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product;
+        }
+    },
 })
